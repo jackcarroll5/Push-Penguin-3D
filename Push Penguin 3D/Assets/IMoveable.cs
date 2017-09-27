6342356df32974f06eb235567927f48c19e83ecd
Christian Jakobi <t00198403@365s.ittralee.ie>
2017-09-27 10:52:27 +0100

    Impemented IMovable  Interface and IceBlock Control, added control Scripts for most modules

---
+++ Push Penguin 3D/Assets/IMoveable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+interface IMoveable
+    {
+
+       void push(Vector3 pusherPosition);
+
+
+
+     //   Hit OnPush(); // CheckForMovement
+                      //Int32 MovementSpeed{get;set;}
+                      //Boolean StartMoving();
+                      //CheckForAction(); / ChangeStatus(); / Collision();
+    }
+
---
+++ Push Penguin 3D/Assets/enumHit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+    enum Hit
+    {
+        none,
+        move,
+        stop,
+        destory,
+        kill
+    }
+
