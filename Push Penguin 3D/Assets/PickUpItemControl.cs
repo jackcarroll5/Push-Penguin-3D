bc76277bc57100bca8a2b0ca17d78cb0344ea521
STUDENTS\T00198405 <laura.braak@students.ittralee.ie>
2017-10-06 13:03:50 +0100

    no message

--- Push Penguin 3D/Assets/PickUpItemControl.cs
+++ Push Penguin 3D/Assets/PickUpItemControl.cs
@@ -2,15 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PickUpItemControl : MonoBehaviour {
+public class PickUpItemControl : MonoBehaviour, IPoints {
 
-	// Use this for initialization
-	void Start () {
+    private int _points = 100;
+    private int minimumPoints = 50;
+    private int maximumPoints = 250;
+    public Transform Popup;
+
+    GameManagerControl theManager;
+    public int points
+    {
+        get
+        {
+            return _points;
+        }
+
+        set
+        {
+            if(value <= minimumPoints)
+            {
+                _points = minimumPoints;
+            } else if (value >= maximumPoints)
+            {
+                _points = maximumPoints;
+            } else
+            {
+                _points = value;
+            }
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<PenguinControl>())
+        {
+         Transform newPopup =   Instantiate(Popup, transform.position, Quaternion.identity);
+            newPopup.GetComponent<PopUpScoreControl>().WithScoreOf(_points);
+            theManager.iveBeenDestroyed(this);
+            Destroy(gameObject);
+        }
+
+
+    }
+    // Update is called once per frame
+    void Update () {
+  
 	}
 }
