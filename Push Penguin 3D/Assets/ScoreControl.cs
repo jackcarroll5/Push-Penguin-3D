280cf5adc69dcdb1b26ddcec45fa77481bfd2150
STUDENTS\t00195739 <lee.osullivan1998@gmail.com>
2017-10-06 12:52:35 +0100

    ScoreControl

--- Push Penguin 3D/Assets/ScoreControl.cs
+++ Push Penguin 3D/Assets/ScoreControl.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ScoreControl : MonoBehaviour, iScore {
-
+    int Challenge1 = 5;
     int currentScore;
 
 
@@ -16,11 +16,29 @@ public class ScoreControl : MonoBehaviour, iScore {
 
     // Use this for initialization
     void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        //Spawning cubes to test score
+        while (Challenge1 > 0)
+        {
+            GameObject Cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            Cube.name = "Enemy " + Challenge1;
+            Cube.transform.position += new Vector3(2 * Challenge1, 0, 0);
+            Challenge1--;
+            Cube.tag = "Test";
+        }
+        
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //getting the cubes to move for score incrementation test
+        if (gameObject.tag == "Test")
+        {
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                this.transform.position += new Vector3(5, 0, 0);
+
+            }
+        }
+    }
 }
