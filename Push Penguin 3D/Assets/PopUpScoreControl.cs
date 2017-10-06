47b8da9d290123179d5a2576d73d40004c7c422e
STUDENTS\t00195834 <John.T.WalshOReiily@Students.ittralee.ie>
2017-10-06 12:55:38 +0100

    Updated

--- Push Penguin 3D/Assets/PopUpScoreControl.cs
+++ Push Penguin 3D/Assets/PopUpScoreControl.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PopUpScoreControl : MonoBehaviour, IPopUp {
 
 
     int myScore;
+    GameObject g;
+    int number; 
 
 
     public void WithScoreOf(int score)
     {
         myScore = score;
+      
     }
 
     //public void example()
@@ -21,13 +25,29 @@ public class PopUpScoreControl : MonoBehaviour, IPopUp {
     // Use this for initialization
     void Start ()
     {
-		
+        
+        g = transform.GetChild(0).GetChild(0).gameObject;
+        g.GetComponent<Text>().text =  "" + RandomNumGen(1,255);
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+        //todo Add alpha detection
+        if()
+        {
+
+        }
+    }
+    List<int> usedValues = new List<int>();
+    private int RandomNumGen(int min, int max)
+    {
+        int val = Random.Range(min, max);
+        while(usedValues.Contains(val))
+        {
+            val = Random.Range(min, max);
+        }
+        return val;
+    }
 }
 //https://www.youtube.com/watch?v=fbUOG7f3jq8
