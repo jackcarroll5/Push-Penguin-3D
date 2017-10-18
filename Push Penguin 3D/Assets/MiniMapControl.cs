bb342338a2d1eb8433c55017a107fb13175359d3
ShaneMorley <shanemorley133@gmail.com>
2017-10-18 10:45:52 +0100

    animation update

---
+++ Push Penguin 3D/Assets/MiniMapControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapControl : MonoBehaviour,IMiniMap {
+
+    public GameObject player;
+    private Vector3 offset;
+    Camera ourCamera;
+
+    float heightOfCamera = 5.0f;
+  
+
+    public void Over(Vector3 focusOfCamera)
+    {
+        transform.position = focusOfCamera + heightOfCamera * Vector3.up;
+      
+    }
+
+    // Use this for initialization
+    void Start () {
+        ourCamera = GetComponent<Camera>();
+
+        if (ourCamera) print("Yippeeee");
+        else print("ouch");
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.transform.position + offset;
+        
+	}
+}
