1eef46a1069250b703900e4af548837e16580e1b
STUDENTS\t00195739 <lee.osullivan@students.ittralee.ie>
2017-10-18 10:47:53 +0100

    no message

--- Push Penguin 3D/Assets/ScoreControl.cs
+++ Push Penguin 3D/Assets/ScoreControl.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 public class ScoreControl : MonoBehaviour, iScore {
 
     int currentScore;
+    GameObject Cube1;
+    private int Increment = 0, Validator = 1;
+    TextMesh tree1;
+
 
 
     public void AddScore(int scoreIncrement)
@@ -16,11 +20,39 @@ public class ScoreControl : MonoBehaviour, iScore {
 
     // Use this for initialization
     void Start () {
-		
+        Cube1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        tree1 = GetComponent<TextMesh>();
+        tree1.transform.position = new Vector3(5, 5, 0);
+        tree1.text = "Score: " + Increment;
+        tree1.fontSize = 255;
+        tree1.characterSize = .03f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+		if(Input.GetKeyDown(KeyCode.Space) && Validator == 1)
+        {
+            Destroy(Cube1);
+            Increment += 100;
+            Validator = 0;
+            Cube1.transform.position = new Vector3(0, 0, 0);
+            tree1.text = "Score: " + Increment;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && Validator == 0)
+        {
+            print("No 1");
+            print(Increment);
+        }
+
+        if(Input.GetKeyDown(KeyCode.S) && Validator == 0)
+        {
+            Cube1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            Validator = 1;
+        }
+        else if(Input.GetKeyDown(KeyCode.S) && Validator == 1)
+        {
+            print("No");
+            print(Increment);
+        }
+    }
 }
