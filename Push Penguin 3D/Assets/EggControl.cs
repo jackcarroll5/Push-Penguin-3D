27fcaf9738e9f0be469ae3e71bfbe8d8ccedb623
STUDENTS\t00196165 <shanemorley133@gmail.com>
2017-10-25 10:28:29 +0100

    full enemy spawn

--- Push Penguin 3D/Assets/EggControl.cs
+++ Push Penguin 3D/Assets/EggControl.cs
@@ -7,33 +7,39 @@ public class EggControl : MonoBehaviour
 {
 
     public bool NotInIce, IceDestroyed;
-    public float TimeCount, Timer = 3;
+    public float Timer = 3;
     public GameObject Enemy;
     Vector3 CurrentPosition;
     GameManagerControl theManager;
+     
     // Variables Declared
     void Start()
     {
         NotInIce = true;
         IceDestroyed = false;
-        TimeCount = Time.time + Timer;
+        
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        Timer -= Time.deltaTime; 
         SpawnEnemy();
     }
 
     void SpawnEnemy()
     {
-        if (Time.time > TimeCount && NotInIce == true)
+        if (Timer <= 0 && NotInIce == true)
         {
 
             CurrentPosition = transform.position;
-            Instantiate(Enemy, CurrentPosition, Quaternion.identity);
+            GameObject EnemyClone =  Instantiate(Enemy, CurrentPosition, Quaternion.identity);
             Destroy(this.gameObject);
+            NPCControl enemyController = EnemyClone.GetComponent<NPCControl>();
+            theManager.EnemySpawn(enemyController);
+            theManager.EggDestroyed(this);
+            
         }
 
         if (IceDestroyed == true && NotInIce == true)
@@ -58,7 +64,7 @@ public class EggControl : MonoBehaviour
         print("NOT");
         if (other.tag == "IceBlock")
             NotInIce = true;
-        TimeCount = 0;
+        Timer = 3;
 
     }
 
